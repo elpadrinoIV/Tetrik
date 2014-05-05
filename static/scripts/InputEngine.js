cb741c4feee7609b1f3b0bae7fe0e575cc59bc26
Nicolas Dascanio <nd@nd.com>
2014-05-05 00:57:21 -0300

    input engine toma eventos de teclado y los asocia a eventos

---
+++ static/scripts/InputEngine.js
@@ -0,0 +1,38 @@
+InputEngine = Class.extend({
+    bindings: {},
+
+    actions: {},
+
+    setup: function() {
+        this.bind(81, 'rotate');
+        this.bind(68, 'move-left');
+        this.bind(71, 'move-right');
+        this.bind(70, 'move-down');
+        //gRenderEngine.canvas.addEventListener("mousemove", this.onMouseMove);
+        //gRenderEngine.canvas.addEventListener("keydown", this.onKeyDown);
+        window.addEventListener("keydown", this.onKeyDown);
+        window.addEventListener("keyup", this.onKeyUp);
+        gRenderEngine.canvas.addEventListener("keyup", this.onKeyUp);
+        console.log(gRenderEngine.canvas);
+    },
+
+    onKeyDown: function(event) {
+        var action = gInputEngine.bindings[event.keyCode];
+        if (action) {
+            gInputEngine.actions[action] = true;
+        }
+    },
+
+    onKeyUp: function(event) {
+        var action = gInputEngine.bindings[event.keyCode];
+        if (action) {
+            gInputEngine.actions[action] = false;
+        }
+    },
+
+    bind: function(key, action) {
+        this.bindings[key] = action;
+    },
+});
+
+var gInputEngine = new InputEngine();
